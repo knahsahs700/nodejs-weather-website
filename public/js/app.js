d6a774cd5373383f7d92e70a480ec023c39017f8
Shashank Shekhar <knahsahs700@gmail.com>
2020-04-11 12:58:14 +0530

    Data increased

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,41 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const searchedLoaction = document.querySelector('#location')
+const temperature = document.querySelector('#temperature')
+const humidity = document.querySelector('#humidity')
+const wind_degree = document.querySelector('#wind-degree')
+const weather_description = document.querySelector('#weather-description')
+const last_observed = document.querySelector('#last-observed')
+const message = document.querySelector('#message')
+const weatherImg = document.querySelector('#weather-img')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const location = search.value
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
+    message.textContent = 'Loading...'
+    searchedLoaction.textContent = ''
+    temperature.textContent = ''
+    humidity.textContent = ''
+    wind_degree.textContent = ''
+    weather_description.textContent = ''
+    last_observed.textContent = ''
+    weatherImg.src = ''
+    const area = search.value
+    fetch('/weather?address=' + area).then((response) => {
+        response.json().then((forecastData) => {
+
+            console.log(forecastData)
+            if (forecastData.error) {
+                message.textContent = forecastData.error
             } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                message.textContent = ''
+                weatherImg.src = forecastData.weatherIcon
+                searchedLoaction.textContent = forecastData.location
+                temperature.textContent = forecastData.temperature + "°C"
+                humidity.textContent = 'Humidity: ' + forecastData.humidity
+                wind_degree.textContent = 'Wind Degree: ' + forecastData.wind_degree
+                weather_description.textContent = forecastData.weather_description
+                last_observed.textContent = 'Last Observed: ' + forecastData.last_observed
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
 
-    url = 'http://api.weatherstack.com/current?access_key=99b3f4664e51ec83e6482b6d689c192d&query=' + lat + '&' + long
+    url = 'http://api.weatherstack.com/current?access_key=99b3f4664e51ec83e6482b6d689c192d&query=' + lat + ',' + long
 
     request({ url, json: true }, (error, { body }) => {
 
@@ -11,7 +11,14 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + "!. It is currently " + body.current.temperature + " degrees out. There is a " + body.current.precip + "% chance of rain.")
+            callback(undefined, {
+                weatherIcon: body.current.weather_icons[0],
+                temperature: body.current.temperature,
+                weather_description: body.current.weather_descriptions[0],
+                wind_degree: body.current.wind_degree,
+                humidity: body.current.humidity,
+                last_observed: body.current.observation_time
+            })
         }
     })
 }
